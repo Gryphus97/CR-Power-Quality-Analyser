1f3b118cdd6a2924f734d064f3e10fe83705b662
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-11-26 21:21:25 -0600

    Update monofasicos.py

--- monofasicos.py
+++ monofasicos.py
@@ -1,25 +1,45 @@
 import pandas as pd
+datos = []
+for i in range(56,76):
+    nom = f"M-07{i}-2018"
+    datos.append(nom)
+datos.remove("M-0772-2018")
+coor = []
+control = pd.read_csv('Test1.csv', delimiter=',', decimal=".")
+for x in range(0,1264):
+    for i in range(0,19):
+        if control.iloc[x, 0] == datos[i]:
+            coor.append(control.iloc[x, 8])
+n_listaN = []
+n_listaO = []
+for i in range(0,19):
+    lista = list(coor[i])
+    if lista[0] == "N":
+        lista.pop(0)
+        for i in range(0,10):
+            lista.pop()
+    n_listaN.append(''.join(lista))
+for i in range(0, 19):
+    lista = list(coor[i])
+    if lista[10] == "O" or lista[10] == "N":
+        lista[10] = "-"
+        for i in range(0,9):
+            lista.pop(0)
+    n_listaO.append(''.join(lista))
+list_data = []
+i = 0
+for filename in datos:
+    data = pd.read_csv(f"{filename}.csv")
+    data.insert(2, "lat", n_listaN[i], allow_duplicates=False)
+    data.insert(3, "lon", n_listaO[i], allow_duplicates=False)
+    list_data.append(data)
+    i+=1
+listo = pd.concat(list_data, ignore_index=True)
 
-datos1 = pd.read_csv('M-0565-2022.csv', delimiter=',', decimal=".")
-datos2 = pd.read_csv('M-0566-2022.csv', delimiter=',', decimal=".")
-datos3 = pd.read_csv('M-0567-2022.csv', delimiter=',', decimal=".")
-datos4 = pd.read_csv('M-0568-2022.csv', delimiter=',', decimal=".")
-datos1.insert(2, "lat", "9.848634400644745", allow_duplicates=False)
-datos1.insert(3, "lon", "-84.31491096137044", allow_duplicates=False)
-datos2.insert(2, "lat", "10.71553", allow_duplicates=False)
-datos2.insert(3, "lon", "-85.62812", allow_duplicates=False)
-datos3.insert(2, "lat", "09.95461", allow_duplicates=False)
-datos3.insert(3, "lon", "-85.03797", allow_duplicates=False)
-datos4.insert(2, "lat", "09.99357", allow_duplicates=False)
-datos4.insert(3, "lon", "-85.28716", allow_duplicates=False)
-datos5 = pd.concat([datos1, datos2, datos3, datos4])
-
-datos_completos = pd.DataFrame(datos5)
+datos_completos = pd.DataFrame(listo)
 datos_completos = datos_completos.set_index('Time', append=False, drop=False)
 fecha_in = []
-fecha_in = []
-lista = list(datos_completos.iloc[3,0])
-for i in range(0, 1439):
+for i in range(0, 19960):
     lista = list(datos_completos.iloc[i, 0])
     for x in range(0,10):
         if lista[1] == str(x):
@@ -27,7 +47,8 @@ for i in range(0, 1439):
                 for m in range(0,10):
                     if lista[12] == str(h) and lista[13] == str(m) and lista[15] == "0" and lista[16] == "0":
                         fecha_in.append(''.join(lista))
-datos_tiempo = datos_completos.loc[fecha_in,["Time", "lat", "lon","Phase C-A Avg Volts"]]
+
+datos_tiempo = datos_completos.loc[fecha_in,["Time", "lat", "lon","Phase C-A Min Volts"]]
 final = datos_completos.loc[fecha_in,"Time"]
 final = final.to_numpy().tolist()
 datos_tiempo.insert(1, "End", final, allow_duplicates=False)
@@ -36,26 +57,16 @@ n_listaT = []
 n_listaE = []
 for i in range(0, tamaño):
     lista = list(datos_tiempo.iloc[i, 0])
-    if lista[3] == "O" and lista[4] == "c" and lista[5] == "t":
-        lista[3] = "10"
-        lista.pop(4)
-        lista.pop(5)
-        lista[4] = "-"
-    elif lista[3] == "N" and lista[4] == "o" and lista[5] == "v":
-        lista[3] = "11"
+    if lista[3] == "J" and lista[4] == "u" and lista[5] == "l":
+        lista[3] = "07"
         lista.pop(4)
         lista.pop(5)
         lista[4] = "-"
     n_listaT.append(''.join(lista))
 for i in range(0, tamaño):
     lista = list(datos_tiempo.iloc[i, 1])
-    if lista[3] == "O" and lista[4] == "c" and lista[5] == "t":
-        lista[3] = "10"
-        lista.pop(4)
-        lista.pop(5)
-        lista[4] = "-"
-    elif lista[3] == "N" and lista[4] == "o" and lista[5] == "v":
-        lista[3] = "11"
+    if lista[3] == "J" and lista[4] == "u" and lista[5] == "l":
+        lista[3] = "07"
         lista.pop(4)
         lista.pop(5)
         lista[4] = "-"
@@ -63,10 +74,9 @@ for i in range(0, tamaño):
 
 datos_tiempo.loc[:,"Time"] = n_listaT
 datos_tiempo.loc[:,"End"] = n_listaE
-
 n9 = []
 n8 = []
-for i in range (0, 953):
+for i in range (0, 59917):
     if float(datos_tiempo.iloc[i,4]) > 113:
         n9.append(datos_tiempo.iloc[i,:])
     else:
@@ -88,5 +98,3 @@ workbook9 =  Workbook("n9.xlsx")
 # Save XLSX as CSV
 workbook8.save("n8.csv" , SaveFormat.CSV)
 workbook9.save("n9.csv" , SaveFormat.CSV)
-
-
