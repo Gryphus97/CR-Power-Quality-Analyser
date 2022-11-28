d316c942ade0d680419d10a0f9395de19e62c108
Krs106 <98362978+Krs106@users.noreply.github.com>
2022-11-27 21:08:11 -0600

    Update tiempo_instalacion.py
    Comentarios

--- tiempo_instalacion.py
+++ tiempo_instalacion.py
@@ -4,16 +4,17 @@ import asposecells
 jpype.startJVM()
 from asposecells.api import Workbook, SaveFormat
 
-# Create a Workbook object with Excel file's path
+#Se convierte el archivo Test.xlsx a formato .csv
 workbook =  Workbook("Test1.xlsx")
-
-# Save XLSX as CSV
 workbook.save("Test1.csv", SaveFormat.CSV)
+#Se crea un dataframe con el archivo creado
 datos = pd.read_csv('Test1.csv', delimiter=',', decimal=".")
 datos_completos = pd.DataFrame(datos)
+#Se escogen las columnas a analizar
 datos_tiempo = datos_completos.loc[:,["Fecha_de_instalación","Hora_instalación","Fecha_de_desinstalación", "Hora_desinstalación", "N° de equipo"]]
 coor = datos_completos.loc[:,"Coordenadas"]
 tamaño = datos_completos.loc[:,"Coordenadas"].size
+#Se convierten las coordenadas a un formato aceptable en Qgis
 n_listaN = []
 n_listaO = []
 for i in range(0,1264):
@@ -37,7 +38,7 @@ datos_tiempo.insert(4, "lat", n_listaN, allow_duplicates=False)
 datos_tiempo['Fecha de instalacion'] = datos_tiempo.Fecha_de_instalación.str.cat(datos_tiempo.Hora_instalación, sep=' ')
 datos_tiempo['Fecha de desinstalacion'] = datos_tiempo.Fecha_de_desinstalación.str.cat(datos_tiempo.Hora_desinstalación, sep=' ')
 datos_tiempo = datos_tiempo.drop(columns=["Fecha_de_instalación", "Hora_instalación", "Fecha_de_desinstalación", "Hora_desinstalación"])
-
+#Se convierten los datos a un archivo .xlsl y porteriormente a uno .csv
 datos_tiempo.to_excel(r'tiempo_ins.xlsx', index = False)
 
 
